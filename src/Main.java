0a8d0bf91f1ce075d90f6238610bd9aa8b220689
Andrey Globa <Globa.Andrey@gmail.com>
2020-03-23 22:59:44 +0300

    Checked the card 5405 1028 9909 0488. Result: OK

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         // TODO: подставлять номер карты нужно сюда между двойными кавычками, без пробелов
-        String number = "5218416379393551";
+        String number = "5405102899090488";
         System.out.println(String.format("Result is %s", isValidCardNumber(number) ? "OK" : "FAIL"));
     }
 
